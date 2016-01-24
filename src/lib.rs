696b923d619d08502d1f5389ed69625db46be93d
Jordan MacDonald <jordan@wastedintelligence.com>
2016-01-23 23:43:44 -0500

    Disable ownership-dependent modules.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! Luthor provides a collection of lexers for various formats and languages.
 //! It also exposes types that aid in building lexers of your own.
-pub mod lexers;
+// pub mod lexers;
 pub mod token;
-mod tokenizer;
+// mod tokenizer;
 
-pub use tokenizer::{Tokenizer, StateFunction};
+// pub use tokenizer::{Tokenizer, StateFunction};
