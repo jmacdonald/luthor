396aca2c0781177d9a5bdcfd9b348125ba9654f1
Jordan MacDonald <jordan@wastedintelligence.com>
2016-05-12 21:18:24 -0400

    Disable lexers in anticipation of &str-based tokenizer implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! Luthor provides a collection of lexers for various formats and languages.
 //! It also exposes types that aid in building lexers of your own.
-pub mod lexers;
+// pub mod lexers;
 pub mod token;
 mod tokenizer;
 
